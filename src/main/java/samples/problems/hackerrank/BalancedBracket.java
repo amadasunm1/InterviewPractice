d9871c834fab07aae03e0030717d32202334729f
Michael Amadasun <amadasunm1@gmail.com>
2020-02-01 23:34:07 -0500

    Balanced Bracket Hacker Rank Solution

---
+++ src/main/java/samples/problems/hackerrank/BalancedBracket.java
@@ -0,0 +1,45 @@
+package samples.problems.hackerrank;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/** Based on HackerRank problem found here: https://www.hackerrank.com/challenges/balanced-brackets/problem**/
+public class BalancedBracket {
+
+    public String  isBalanced(String string) {
+        if(string.isBlank())
+            return "NO";
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (Character c: string.toCharArray()) {
+            switch (c){
+                case '{':
+                case '(':
+                case '[':
+                    stack.addFirst(c);
+                    break;
+                case '}':
+                    if (!isMatch(stack, '{')) return "NO";
+                    break;
+                case ')':
+                    if (!isMatch(stack, '(')) return "NO";
+                    break;
+                case ']':
+                    if (!isMatch(stack, '[')) return "NO";
+                    break;
+                default:
+                    return "NO";
+            }
+        }
+
+        if (stack.isEmpty())
+            return "YES";
+
+        return "NO";
+    }
+
+    private boolean isMatch(Deque<Character> stack, Character character) {
+        return !stack.isEmpty() && stack.removeFirst().equals(character);
+    }
+}
---
+++ src/test/java/samples/problems/hackerrank/BalancedBracketTest.java
@@ -0,0 +1,17 @@
+package samples.problems.hackerrank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BalancedBracketTest {
+
+    @Test
+    public void isBalanced() {
+        BalancedBracket balancedBracket = new BalancedBracket();
+        assertEquals("NO", balancedBracket.isBalanced(""));
+        assertEquals("YES", balancedBracket.isBalanced("{[()]}"));
+        assertEquals("NO", balancedBracket.isBalanced("{[(])}"));
+        assertEquals("YES", balancedBracket.isBalanced("{{[[(())]]}}"));
+    }
+}
